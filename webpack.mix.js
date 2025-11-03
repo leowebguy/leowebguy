3fbdae0d432065dbbe232e1b62a936a37fbf0e1e
Leo Leoncio <leowebguy@gmail.com>
2025-11-03 10:15:22 -0500

    ...

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ const mix = require('laravel-mix'),
     pkg = require('./package.json'),
     fs = require('fs-extra'),
     path = require('path'),
-    // manifest = require('./public/mix-manifest.json'),
     handlebars = require('handlebars-webpack-plugin'),
     gitRevSync = require('git-rev-sync');
 
@@ -17,7 +16,7 @@ require('laravel-mix-purgecss');
 
 mix
 
-    .setPublicPath(pkg.paths.public)
+    .setPublicPath(pkg.paths.dist)
 
     .disableNotifications()
 
@@ -39,20 +38,21 @@ mix
         //     ]
         // },
         devServer: {
-            // open: true,
             // server: 'https',
             host: 'localhost',
             port: 8080,
-            https: {
-                cert: fs.readFileSync(path.join(__dirname, pkg.paths.ssl, 'cert.pem')),
-                key: fs.readFileSync(path.join(__dirname, pkg.paths.ssl, 'key.pem'))
-            },
+            // https: {
+            //     cert: fs.readFileSync(path.join(__dirname, pkg.paths.ssl, 'cert.pem')),
+            //     key: fs.readFileSync(path.join(__dirname, pkg.paths.ssl, 'key.pem'))
+            // },
+            // open: true,
+            watch: true,
             liveReload: true
         },
         plugins: [
             new handlebars({
                 entry: path.join(__dirname, pkg.paths.src, '*.hbs'),
-                output: path.join(__dirname, pkg.paths.public, '[name].html'),
+                output: path.join(__dirname, pkg.paths.dist, '[name].html'),
                 data: {
                     manifestJs: `/js/manifest.js?v=${t}`,
                     vendorJs: `/js/vendor.js?v=${t}`,
@@ -70,9 +70,9 @@ mix
     //     jquery: ['$', 'window.jQuery']
     // })
 
-    .js(`${pkg.paths.src}/js/app.js`, `${pkg.paths.public}/js/app.js`)
+    .js(`${pkg.paths.src}/js/app.js`, `${pkg.paths.dist}/js/app.js`)
 
-    .sass(`${pkg.paths.src}/scss/app.scss`, `${pkg.paths.public}/css/app.css`)
+    .sass(`${pkg.paths.src}/scss/app.scss`, `${pkg.paths.dist}/css/app.css`)
 
     .extract()
 
@@ -87,34 +87,34 @@ if (mix.inProduction()) {
         .before(() => {
             fs.copySync(
                 path.join(__dirname, pkg.paths.npm, 'bootstrap-icons/font/fonts'),
-                path.join(__dirname, pkg.paths.public, 'fonts/bi')
+                path.join(__dirname, pkg.paths.dist, 'fonts/bi')
             );
             fs.copySync(
                 path.join(__dirname, pkg.paths.src, 'svg'),
-                path.join(__dirname, pkg.paths.public, 'svg')
+                path.join(__dirname, pkg.paths.dist, 'svg')
             );
             fs.copySync(
                 path.join(__dirname, pkg.paths.src, 'img'),
-                path.join(__dirname, pkg.paths.public, 'img')
+                path.join(__dirname, pkg.paths.dist, 'img')
             );
             fs.copySync(
                 path.join(__dirname, pkg.paths.src, 'fav'),
-                path.join(__dirname, pkg.paths.public, 'fav')
+                path.join(__dirname, pkg.paths.dist, 'fav')
             );
             fs.copy(
                 path.join(__dirname, pkg.paths.src, 'CNAME'),
-                path.join(__dirname, pkg.paths.public, 'CNAME')
+                path.join(__dirname, pkg.paths.dist, 'CNAME')
             );
             fs.copy(
-                path.join(__dirname, pkg.paths.src, 'favicon.ico'),
-                path.join(__dirname, pkg.paths.public, 'favicon.ico')
+                path.join(__dirname, pkg.paths.src, 'fav/favicon.ico'),
+                path.join(__dirname, pkg.paths.dist, 'favicon.ico')
             );
         })
 
-        // .purgeCss({
-        //     content: [`${pkg.paths.src}/**/*.hbs`],
-        //     whitelistPatterns: [/d-$/, /modal-$/, /fade/, /show/]
-        // })
+        .purgeCss({
+            content: [`${pkg.paths.src}/**/*.hbs`]
+            // whitelistPatterns: [/d-$/, /modal-$/, /fade/, /show/]
+        })
 
         .compress()
 
